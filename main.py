83e12f169bfb398b4ddf4df03f5fae1271347f2d
Connor Keevill <connor.keevill.19@gmail.com>
2023-12-12 22:52:57 +0000

    Set up audio endpoint to now perform the entire pipeline (audio -> text -> audio). Note that this will have broken some conversational stuff - but this is ok for the MVP

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from io import BytesIO
 from uuid import uuid4
 from fastapi import FastAPI, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
 from openai import OpenAI
 from pydantic import BaseModel
 
@@ -20,19 +21,36 @@ app.add_middleware(
 OPENAI_API_KEY = "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"
 
 
+class MessageOut(BaseModel):
+    id: str
+    role: str
+    content: str
+
+
+class MessageIn(BaseModel):
+    content: str
+
+recordings: dict[str, BytesIO] = {}
+
 class Conversation(BaseModel):
     id: str
-    messages: list[dict]
+    messages: list[MessageOut]
     with_who: str
+
+    def __contains__(self, item) -> bool:
+        return item in [message.id for message in self.messages]
+
+    def get_message(self, message_id):
+        if message_id not in self:
+            raise ValueError(f"Message with ID {message_id} does not exist.")
+
+        for message in self.messages:
+            if message.id == message_id:
+                return message
 
 
 class Name(BaseModel):
     name: str
-
-
-class Message(BaseModel):
-    content: str
-
 
 
 conversations: dict[str, Conversation] = {}
@@ -40,7 +58,7 @@ gpt = OpenAI(api_key=OPENAI_API_KEY)
 
 
 @app.get("/conversations")
-async def get_conversations():
+async def get_conversations() -> dict[str, Conversation]:
     """
     Get all conversations.
 
@@ -84,7 +102,7 @@ async def create_conversation(name: Name):
 
 
 @app.get("/conversations/{conversation_id}/messages", response_model=list)
-async def get_conversation_messages(conversation_id: str):
+async def get_all_conversation_messages(conversation_id: str):
     """
     Retrieve a list of messages from a conversation.
 
@@ -97,13 +115,13 @@ async def get_conversation_messages(conversation_id: str):
     return conversations[conversation_id].messages
 
 
-@app.get("/conversations/{conversation_id}/messages/{message_index}")
-async def get_conversation_message(conversation_id: str, message_index: int):
+@app.get("/conversations/{conversation_id}/messages/{message_id}/text")
+async def get_text_conversation_message(conversation_id: str, message_id: str):
     """
     Retrieves a specific message from a conversation.
 
     :param conversation_id: The ID of the conversation.
-    :param message_index: The index of the message in the conversation.
+    :param message_id: The ID of the message in the conversation.
     :return: The requested message.
     :raises HTTPException: If the conversation or message index is not found.
     """
@@ -112,14 +130,37 @@ async def get_conversation_message(conversation_id: str, message_index: int):
 
     conversation = conversations[conversation_id]
 
-    if message_index > len(conversation.messages) + 1:
-        raise HTTPException(status_code=404, detail="Message index out of range.")
+    if message_id not in conversation:
+        raise HTTPException(status_code=404, detail="Message not found")
 
-    return conversation.messages[message_index]
+    return conversation.get_message(message_id)
+
+
+@app.get("/conversations/{conversation_id}/messages/{message_id}/audio")
+async def get_audio_conversation_message(conversation_id: str, message_id: str) -> StreamingResponse:
+    """
+    Retrieves the audio associated with a specific message from a conversation
+
+    :param conversation_id: the ID of the conversation of interest.
+    :param message_id: the ID of the message of interest.
+    :return: the audio of this message.
+    """
+    if conversation_id not in conversations:
+        raise HTTPException(status_code=404, detail="Conversation not found.")
+
+    conversation = conversations[conversation_id]
+
+    if message_id not in recordings:
+        raise HTTPException(status_code=404, detail="Message audio not found")
+
+    audio_bytes = recordings[message_id]
+    audio_bytes.seek(0)
+
+    return StreamingResponse(audio_bytes, media_type="audio/wav")
 
 
 @app.post("/conversations/{conversation_id}/messages/text")
-async def create_text_conversation_message(conversation_id: str, message: Message):
+async def create_text_conversation_message(conversation_id: str, message: MessageIn) -> MessageOut:
     """
     Stores the given message in the conversation, submits the message to GPT, also stores the response in the
     conversation, and returns a response with the whole conversation history, as well as the individual message.
@@ -133,22 +174,24 @@ async def create_text_conversation_message(conversation_id: str, message: Messag
 
     conversation: Conversation = conversations[conversation_id]
 
+    userMessage = MessageOut(
+        id=str(uuid4()),
+        role="user",
+        content=message.content
+    )
+
     gpt_message = get_gpt_reply(conversation, message)
 
-    return {
-        "conversation": conversation,
-        "reply": gpt_message
-    }
+    conversation.messages.append(userMessage)
+    conversation.messages.append(gpt_message)
+
+    return gpt_message
 
 
 @app.post("/conversations/{conversation_id}/messages/audio")
-async def create_audio_conversation_message(conversation_id: str, recording: UploadFile):
-    print("We are in the correct handler")
-
+async def create_audio_conversation_message(conversation_id: str, recording: UploadFile) -> MessageOut:
     if recording.content_type not in ["audio/wav", "audio/x-wav"]:
         raise HTTPException(status_code=415, detail="Unsupported audio format. Please upload a WAV file.")
-
-    print("file validated")
 
     if conversation_id not in conversations:
         raise HTTPException(status_code=404, detail="Conversation not found.")
@@ -160,18 +203,27 @@ async def create_audio_conversation_message(conversation_id: str, recording: Upl
     audio.name = "audio.wav"
 
     transcript = gpt.audio.transcriptions.create(model="whisper-1", file=audio)
-
     print("transcript:", transcript)
 
-    gpt_message = await get_gpt_reply(conversation, transcript)
+    message = get_gpt_reply(conversation, transcript)
 
-    return {
-        "conversation": conversation,
-        "reply": gpt_message
-    }
+    audio = gpt.audio.speech.create(model="tts-1", voice="echo", input=message.content)
+
+    conversation.messages.append(message)
+
+    audio_store = BytesIO()
+    audio_store.name = message.id + "mp3"
+
+    for chunk in audio.iter_bytes(chunk_size=1024):
+        audio_store.write(chunk)
+
+    audio_store.seek(0)
+    recordings[message.id] = audio_store
+
+    return message
 
 
-async def get_gpt_reply(conversation, user_message):
+def get_gpt_reply(conversation, user_message) -> MessageOut:
     """
     Adds the given message to the conversation and gets a response from GPT.
 
@@ -183,6 +235,13 @@ async def get_gpt_reply(conversation, user_message):
     response = gpt.chat.completions.create(model="gpt-3.5-turbo", messages=conversation.messages)
     gpt_message = response.choices[0].message.content
     conversation.messages.append({"role": "assistant", "content": gpt_message})
+
+    gpt_message = MessageOut(
+        id=str(uuid4()),
+        role="assistant",
+        content=gpt_message
+    )
+
     return gpt_message
 
 
