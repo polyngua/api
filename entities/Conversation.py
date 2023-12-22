40385c98e78be8fd3105bdf132befc159603ce43
Connor Keevill <connor.keevill.19@gmail.com>
2023-12-22 17:23:25 +0000

    Create Conversation and Message entities, in an attempt to Clean Architect the system. This will be a work in progress (while I am learning the architecture, and also while building the MVP).

---
+++ entities/Conversation.py
@@ -0,0 +1,57 @@
+import uuid
+from typing import Optional
+
+from Message import Message
+
+
+class Conversation:
+    def __init__(self, name: str, system_prompt: str):
+        """
+        A conversation containing messages between the user and the assistant
+
+        :param name:
+        """
+        self.user_name: name
+        self.identifier: str = str(uuid.uuid4())
+        self.messages: dict[str, Message] = {}
+        self.give_message(Message("system", system_prompt))
+
+    def give_message(self, message: Message) -> None:
+        """
+        Adds the given message to this conversation.
+
+        :param message: the message to add to the conversation.
+        """
+        self.messages[message.identifier] = message
+
+    def message_exists(self, identifier: uuid):
+        """
+        :param identifier: identifier of relevant message.
+        :return: boolean indicating whether the message is in this conversation.
+        """
+        return identifier in self.messages.keys()
+
+    def get_message(self, identifier: uuid) -> Optional[Message]:
+        """
+        Returns the specific message specified (if it exists)
+
+        :param identifier: the message to return.
+        :return:
+        """
+        if not self.message_exists(identifier):
+            return None
+
+        return self.messages[identifier]
+
+    def get_all_messages(self) -> dict[str, Message]:
+        """
+        :return: the messages which make up this conversion, barring the first one; that is not part of the conversation
+        but rather the system prompt.
+        """
+        return self.messages[1:]
+
+    def get_system_prompt(self) -> Message:
+        """
+        :return: the system prompt for this conversation.
+        """
+        return next(iter(self.messages.values()))  # Supposedly has a lower overhead than making a list. Makes sense.
