0ca7797a250265ee853a649ba31e97eafd5f120b
Connor Keevill <connor.keevill.19@gmail.com>
2023-12-31 18:31:49 +0000

    Add a field_validator to the conversation DTO to help with processing the messages field

--- services/data_transfer_objects/conversation.py
+++ services/data_transfer_objects/conversation.py
@@ -1,12 +1,25 @@
-from pydantic import BaseModel
+from entities import Message
+from pydantic import BaseModel, Field, field_validator
+from typing import Optional
 
 from . message import *
 
 
 class ConversationBase(BaseModel):
     id: int
-    with_who: str
+    with_who: str = Field(validation_alias="user_name")
+
+
+class ConversationIn(BaseModel):
+    name: str
 
 
 class ConversationOut(ConversationBase):
-    messages: list[MessageOut]
+    messages: Optional[list[MessageOut]]
+
+    @field_validator("messages", mode="before")
+    @classmethod
+    def parse_conversation_messages(cls, messages: dict[int, Message]):
+        print([(message.as_dict()) for message in messages.values()])
+
+        return [(message.as_dict()) for message in messages.values()]
