d0561d70514d524c9c19a046573ff8cb4c0380e0
Connor Keevill <connor.keevill.19@gmail.com>
2024-01-01 12:40:22 +0000

    Alter the parameter name in the  GetConversation dependency

--- dependencies.py
+++ dependencies.py
@@ -9,11 +9,11 @@ class GetConversationDependency:
     def __init__(self, repo: ConversationRepository):
         self.repo = repo
 
-    def execute(self, id: int) -> Conversation:
+    def execute(self, conversation_id: int) -> Conversation:
         """
         Returns the conversation with the given ID, if it exists.
 
         :param id: the id of the conversation to get.
         :return: the conversation.
         """
-        return self.repo.get(id)
+        return self.repo.get(conversation_id)
