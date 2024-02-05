da8cd1a6b61946e5afa7f2cd9a6745d6033817de
Connor Keevill <connor.keevill.19@gmail.com>
2024-02-05 21:41:38 +0000

    Implement GetUserUseCase

--- src/core/services/use_cases/user.py
+++ src/core/services/use_cases/user.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from src.core.entities.user import User, UserRepository
 
 
@@ -10,3 +12,11 @@ class CreateUserUseCase:
         new_user = self.repo.create(user.email, user.first_name, user.surname, password)
 
         return new_user
+
+
+class GetUserUseCase:
+    def __init__(self, user_repository: UserRepository):
+        self.repo = user_repository
+
+    def execute(self, email: str, password: str) -> User:
+        return self.repo.get_by_email_and_password(email, password)
