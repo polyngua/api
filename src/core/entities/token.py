ae59059db5a89e59cc0ce591783a8b70eaf1418e
Connor Keevill <connor.keevill.19@gmail.com>
2024-02-11 15:57:44 +0000

    Create new Token entity

---
+++ src/core/entities/token.py
@@ -0,0 +1,23 @@
+from abc import ABC
+from datetime import datetime
+from uuid import UUID
+
+from src.core.entities import Entity, EntityRepository
+
+
+class Token(Entity):
+    def __init__(self, ID: UUID, token: str, expiration: datetime, user_id: UUID):
+        super().__init__(ID)
+        self.token = token
+        self.expiration = expiration
+        self.user_id = user_id
+
+    def has_expired(self) -> bool:
+        """
+        Returns a boolean indicating whether the token has expired.
+        """
+        return self.expiration < datetime.now()
+
+
+class TokenRepository(EntityRepository, ABC):
+    ...
