edcb220be6bed2a10e787e663cc2834c8c940d3b
Connor Keevill <connor.keevill.19@gmail.com>
2024-02-11 21:36:35 +0000

    Create a new Token table in the database along with a repository for it. Note that there isn't yet an in-memory repository for the tokens. THis is very low on the priority list for now.

--- src/persistence/database/models.py
+++ src/persistence/database/models.py
@@ -1,4 +1,5 @@
 import uuid
+from datetime import datetime
 from typing import Optional
 from uuid import UUID
 
@@ -27,8 +28,8 @@ class Conversation(Base):
     ID: Mapped[UUID] = mapped_column(primary_key=True)
     user_id: Mapped[UUID] = mapped_column(ForeignKey(User.ID))
     system_prompt: Mapped[str] = mapped_column()
-    messages: Mapped[list["Message"]] = relationship()
 
+    messages: Mapped[list["Message"]] = relationship("Message", back_populates="conversation")
     user: Mapped[User] = relationship(User)
 
 
@@ -40,3 +41,16 @@ class Message(Base):
     role: Mapped[str] = mapped_column()  # TODO: This is revealing an implementation detail of the underlying OpenAI API
     content: Mapped[str] = mapped_column()
     audio_filepath: Mapped[Optional[str]] = mapped_column()
+
+    conversation: Mapped[Conversation] = relationship(Conversation, back_populates="messages")
+
+
+class Token(Base):
+    __tablename__ = "tokens"
+
+    ID: Mapped[UUID] = mapped_column(primary_key=True)
+    token: Mapped[str] = mapped_column()
+    user_id: Mapped[UUID] = mapped_column(ForeignKey(User.ID))
+    expires: Mapped[datetime] = mapped_column()
+
+    user: Mapped[User] = relationship(User)
