7759b54ccadce7c4e0cf12d7047fef6deb5764df
Connor Keevill <connor.keevill.19@gmail.com>
2024-02-13 07:42:05 +0000

    Despite the earlier git message, the DB being used was not actually a file-based one. This commit fixes that error

--- src/api/main.py
+++ src/api/main.py
@@ -29,7 +29,7 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-engine = create_engine("sqlite://")
+engine = create_engine("sqlite:///dev.db")
 Session = sessionmaker(bind=engine)
 
 Base.metadata.create_all(bind=engine)
